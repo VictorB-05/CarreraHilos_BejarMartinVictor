cb811ec2c79ab5e408fc036e360b0777b005de2c
victor <>
2024-11-26 17:52:35 +0100

    Se ha mejorado la forma de moverse de los corredores siendo una variación de uno en uno en la velocidad, se han puesto mejor las imagenes, se han añadido un boton para iniciar y otro para poner los corredores al principio de la carrera y se ha puesto un mensaje que indica quien ha ganado y lo recorrido por cada corredor

--- src/Corredor.java
+++ src/Corredor.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 public class Corredor extends JComponent implements Runnable{
     private final Image corredorImage;
     private static boolean fin = false;
+    private int velocidad = 5;
+    private String nombre;
 
     public Corredor(String imagen) throws IOException {
         super();
@@ -14,8 +16,23 @@ public class Corredor extends JComponent implements Runnable{
 
     }
 
+    public Corredor(String imagen,String nombre) throws IOException {
+        super();
+        this.corredorImage = ImageIO.read(Interfaz.class.getResource(imagen));
+        this.setSize(400,300);
+        this.nombre = nombre;
+    }
+
     public static boolean isFin() {
         return fin;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void reIniciar(){
+        fin = false;
     }
 
     @Override
@@ -33,15 +50,42 @@ public class Corredor extends JComponent implements Runnable{
 
     @Override
     public void run() {
-        while(this.getY()<780 && !fin){
+        while(this.getY()<700 && !fin){
             try {
-                this.setLocation(this.getX(), (int) (this.getY()+(Math.random()*10)+1));
+                cambioV();
+                this.setLocation(this.getX(), (this.getY()+velocidad));
                 Thread.sleep(100);
-//                System.out.println("hey"+this.getY());
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
         fin = true;
+    }
+
+    @Override
+    public String toString() {
+        return "Corredor: " +
+                nombre + " recorrido: " + this.getY();
+    }
+
+    private void cambioV() {
+        switch ((int) ((Math.random()*3)+1)){
+            case 1:
+                if(velocidad<10){
+                    velocidad++;
+                }
+                break;
+            case 2:
+                if(velocidad>2){
+                    velocidad--;
+                }
+                break;
+            case 3:
+                if(velocidad<4){
+                    velocidad++;
+                }else if(velocidad>7){
+                    velocidad--;
+                }
+        }
     }
 }
--- src/Interfaz.java
+++ src/Interfaz.java
@@ -1,8 +1,6 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.BevelBorder;
 import java.awt.*;
-import java.io.File;
 import java.io.IOException;
 
 public class Interfaz extends JFrame {
@@ -12,14 +10,19 @@ public class Interfaz extends JFrame {
     Corredor coche2;
     Corredor coche3;
     Corredor coche4;
+    JButton iniciarB;
+    JButton reiniciarB;
+
 
     public Interfaz() throws IOException, InterruptedException {
         super("Carreras hilos");
-        setSize(new Dimension(624,1030));
+        setSize(new Dimension(640,1030));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
         setLayout(null);
+        Image img = ImageIO.read(getClass().getResource("img/icono.png"));
+        setIconImage(img);
         AddElementos();
     }
 
@@ -28,25 +31,55 @@ public class Interfaz extends JFrame {
         // Agregar fondo personalizado
         FondoPanel fondoPanel = new FondoPanel("img/carrera.png");
         fondoPanel.setBounds(0, 0, 624, 1000);
-        fondoPanel.setLayout(null); // Permitir posicionamiento absoluto
+        fondoPanel.setLayout(null); // Permite posicionamiento absoluto
         getContentPane().add(fondoPanel);
 
-        coche1 = new Corredor("img/avaismarus.png");
-        coche1.setBounds(50,30,145,180);
+        // los corredores
+        coche1 = new Corredor("img/avaismarus.png","avasimarus");
         fondoPanel.add(coche1);
 
-        coche2 = new Corredor("img/Luigi.png");
-        coche2.setBounds(190,30,145,180);
+        coche2 = new Corredor("img/Luigi.png","mario and luigi");
         fondoPanel.add(coche2);
 
-        coche3 = new Corredor("img/pibeChill.png");
-        coche3.setBounds(300,30,145,180);
+        coche3 = new Corredor("img/pibeChill.png","chill");
         fondoPanel.add(coche3);
 
-        coche4 = new Corredor("img/miniPekka.png");
-        coche4.setBounds(440,30,145,180);
+        coche4 = new Corredor("img/miniPekka.png","mini P.E.K.K.A.");
         fondoPanel.add(coche4);
 
+        //Boton para iniciar la carrera
+        iniciarB = new JButton();
+        try {
+            Image img = ImageIO.read(getClass().getResource("img/start.png"));
+            iniciarB.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        iniciarB.setVisible(true);
+        iniciarB.addActionListener(e -> correr());
+        iniciarB.setBounds(0,0,60,70);
+        fondoPanel.add(iniciarB);
+
+        //Boton para reinicar las posiciones
+        reiniciarB = new JButton();
+        try {
+            Image img = ImageIO.read(getClass().getResource("img/restart.png"));
+            reiniciarB.setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        reiniciarB.setVisible(true);
+        reiniciarB.addActionListener(e -> posicionInicio());
+        reiniciarB.setBounds(560,0,65,70);
+        reiniciarB.setBackground(Color.green);
+        fondoPanel.add(reiniciarB);
+
+        posicionInicio();
+    }
+
+    private void correr(){
+        reiniciarB.setEnabled(false);
+        iniciarB.setEnabled(false);
         Thread t1 = new Thread(coche1);
         t1.start();
         Thread t2 = new Thread(coche2);
@@ -59,46 +92,68 @@ public class Interfaz extends JFrame {
         t5.start();
     }
 
-    private void win(){
-        while (true){
-            if(Corredor.isFin()){
-                JOptionPane.showMessageDialog(null,"Gano el corredor:"+comprobar());
-                break;
-            }else{
-                System.out.println("paco");
-            }
-        }
-
+    private void posicionInicio(){
+        coche1.reIniciar();
+        coche1.setBounds(60,0,145,200);
+        coche2.setBounds(190,0,145,200);
+        coche3.setBounds(300,0,145,200);
+        coche4.setBounds(440,0,145,200);
+        iniciarB.setEnabled(true);
     }
-    private String comprobar(){
-        if(coche1.getY()>coche2.getY()){
-            if (coche1.getY()>coche3.getY()){
-                if (coche1.getY()>coche4.getY()){
-                    return "coche1";
-                }else{
-                    return "coche4";
-                }
-            }else{
-                if(coche3.getY()>coche4.getY()){
-                    return "coche3";
-                }else{
-                    return "coche4";
+
+    private void win(){
+        try {
+            while (true){
+                Thread.sleep(200);// para limitar las comprobaciones
+                if(Corredor.isFin()){
+                    JOptionPane.showMessageDialog(null,"Gano el corredor:"+resultado());
+                    break;
                 }
             }
-        }else{
-            if (coche2.getY()>coche3.getY()){
-                if (coche2.getY()>coche4.getY()){
-                    return "coche2";
-                }else{
-                    return "coche4";
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        reiniciarB.setEnabled(true);
+    }
+
+    private Corredor comprobar() {
+        if (coche1.getY() > coche2.getY()) {
+            if (coche1.getY() > coche3.getY()) {
+                if (coche1.getY() > coche4.getY()) {
+                    return coche1;
+                } else {
+                    return coche4;
                 }
-            }else{
-                if(coche3.getY()>coche4.getY()){
-                    return "coche3";
-                }else{
-                    return "coche4";
+            } else {
+                if (coche3.getY() > coche4.getY()) {
+                    return coche3;
+                } else {
+                    return coche4;
+                }
+            }
+        } else {
+            if (coche2.getY() > coche3.getY()) {
+                if (coche2.getY() > coche4.getY()) {
+                    return coche2;
+                } else {
+                    return coche4;
+                }
+            } else {
+                if (coche3.getY() > coche4.getY()) {
+                    return coche3;
+                } else {
+                    return coche4;
                 }
             }
         }
+    }
+    private String resultado(){
+        String aux = "";
+        aux += comprobar().getNombre() + "\n";
+        aux += coche1.toString()+ "\n";
+        aux += coche2.toString()+ "\n";
+        aux += coche3.toString()+ "\n";
+        aux += coche4.toString()+ "\n";
+        return aux;
     }
 }
